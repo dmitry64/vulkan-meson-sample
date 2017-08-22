f189473a965dc646c3b738aff94bb6e9113bc899
Dmitry Zykov <dmitriizykov64@gmail.com>
2017-08-22 23:16:24 +0300

    Sample

---
+++ main.cxx
@@ -0,0 +1,14 @@
+#include <vulkan/vulkan.hpp>
+
+int main() {
+	vk::Instance instance;
+
+    vk::ApplicationInfo appInfo;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    return 0;
+}
